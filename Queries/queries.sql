54f20ba94b26198bd83a2ba77fd3660cb93df201
Dinesh Shetty <86020257+dshetty100@users.noreply.github.com>
2021-09-26 01:12:49 -0400

    Added retirement_info.csv and queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -5,7 +5,7 @@ CREATE TABLE departments (
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
-SELECT * FROM departments;
+-- SELECT * FROM departments;
 
 CREATE TABLE employees (
 	 emp_no INT NOT NULL,
@@ -16,6 +16,8 @@ CREATE TABLE employees (
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+-- SELECT * FROM employees;
+
 CREATE TABLE dept_manager (
     dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -25,6 +27,8 @@ CREATE TABLE dept_manager (
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
+-- SELECT * FROM dept_manager;
+
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -33,16 +37,20 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+-- SELECT * FROM salaries;
+
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
+-- SELECT * FROM dept_emp;
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
@@ -52,3 +60,28 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
+-- SELECT * FROM titles;
+
+-- DROP TABLE dept_emp CASCADE;
+
+-- Determining Retirement Eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Creating New Table to store Retirement-Eligible Employee
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Looking at our new table
+SELECT * FROM retirement_info;
